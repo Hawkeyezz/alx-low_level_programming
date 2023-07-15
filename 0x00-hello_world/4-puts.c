06e7363f80d0bd8debaf8e9a0baec4f6b07fffd3
Hawkeyezz <clintonetuk@gmail.com>
2023-07-15 01:01:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle with puts function
+ * Return: 0 (succesful)
+ */
+int main(void)
+{
+puts(**\"Programming is like building a multilingual puzzle*);
+return (0);
+}

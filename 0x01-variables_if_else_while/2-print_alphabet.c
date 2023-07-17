208a7928842ca8fddd6f1a5b30c0846ea3ed38ee
Hawkeyezz <clintonetuk@gmail.com>
2023-07-16 22:02:01 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,16 @@
  */
 
 int main(void)
-{       
-char c; 
+{
+char c;
 
 c = 'a';
 while
-        (c <= 'z') {
-                putchar(c);
-                c++;
-        }       
+(c <= 'z')
+{
+putchar(c);
+c++;
+}
 putchar('\n');
 return (0);
 }

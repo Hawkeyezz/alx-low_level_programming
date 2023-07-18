193298466a08da04871193ab09f2e1569572922a
Hawkeyezz <clintonetuk@gmail.com>
2023-07-17 17:39:05 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - My entry point
+ * Return: Must be 0 (Success)
+ */
+
+int main(void)
+{
+char f;
+
+f = 'z';
+while
+(f >= 'a')
+{
+putchar(f);
+f--;
+}
+putchar('\n');
+return (0);
+}

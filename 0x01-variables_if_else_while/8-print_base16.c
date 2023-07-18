e898f315198ac2ff7c71fbb83868db6bce8a894f
Hawkeyezz <clintonetuk@gmail.com>
2023-07-17 18:15:36 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - My entry point
+ * Return: Must be 0 (Success)
+ */
+
+int main(void)
+{
+char x;
+
+int y;
+
+x = 'a';
+y = 0;
+while
+(y < 10)
+{
+putchar(y + '0');
+y++;
+}
+while
+(x <= 'f')
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return(0);
+}

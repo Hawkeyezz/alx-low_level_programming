e6a8fb4613c6230b488246a932776c162aadb9b1
Hawkeyezz <clintonetuk@gmail.com>
2023-07-31 12:29:51 -0700

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_strings - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer to char
+ */
+
+void set_strings(char **s, char *to)
+{
+	*s = to;
+}

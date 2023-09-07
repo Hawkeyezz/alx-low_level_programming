98630db8f29a6b1f78b7774ed7ac3b3890a7cb7a
Hawkeyezz <clintonetuk@gmail.com>
2023-09-07 13:13:09 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,22 +5,36 @@
  * read_textfile- Reading the text file print, to the STDOUT.
  * @filename: the text file to be read
  * @letters: the number of letters to be read
- * Return: w- actual number of bytes read and printed, 0 when function fails or filename is NULL.
+ * Return: actual number of bytes read and printed, else 0 when function fails or filename is NULL.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char buff;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
-fd = open(filename, O_RDONLY);
-if (fd == -1)
+char *buffer;
+int file_descriptor = -1;
+ssize_t output = 0;
+
+if (!filename)
 return (0);
-buff = malloc(sizeof(char); * letters);
-t = read(fd, buff, letters);
-w = write(STDOUT_FILENO, buff, t);
-free(buff);
-close(fd);
-return (w);
+file_descriptor = open(filename, 0_RDONLY);
+if (file_descriptor < 0)
+return (0);
+buffer = malloc(sizeof(char) * letters);
+if (!buffer)
+{
+close(file_descriptor);
+return (0);
 }
+output = read(file_descriptor, buffer, letters);
+if (output < 0)
+{
+free(buffer);
+close(file_descriptor);
+return (0);
+}
+output = write(STDOUT_FILENO, buffer, output);
+free(buffer);
+close(file_descriptor);
+if (output < 0)
+return (0);
+return (output);
